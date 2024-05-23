6cfeb14a7502b979e2ad81d70f91dc8244557107
dsedlan22 <dsedlan22@student.foi.hr>
2024-05-23 15:25:32 +0200

    Napravljena klasa Student

---
+++ Software/SCVZ Restoraunt/Klase/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollegeRestraunt.classes
+{
+    public class StudentClass
+    {
+        public string ID, JMBAG, ImeIprezime;
+        public StudentClass(string jmbag, string imeiprezime) { JMBAG = jmbag; ImeIprezime = imeiprezime; }
+        public StudentClass(string id, string jmbag, string imeiprezime) { JMBAG = jmbag; ImeIprezime = imeiprezime; ID = id; }
+    }
+
+
+}
