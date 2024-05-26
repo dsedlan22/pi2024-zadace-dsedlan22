f5b183e7d4e6084dc5e06482637f5a6dc679814d
dsedlan22 <dsedlan22@student.foi.hr>
2024-05-27 00:49:16 +0200

    Konekcija

--- Software/SCVZ Restoraunt/Klase/Class1.cs
+++ Software/SCVZ Restoraunt/Klase/Class1.cs
@@ -15,6 +15,44 @@ namespace SCVZ_Restoraunt.Klase
         public List<StudentClass> students = new List<StudentClass>();
         public List<JeloClass> jela = new List<JeloClass>();
 
+        public DBclass()
+        {
 
+            konekcija = new SqlConnection(Connection);
+            try
+            {
+                konekcija.Open();
+                SqlCommand cmd;
+                SqlDataReader reader;
+                cmd = new SqlCommand("Select * from Studenti", konekcija);
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    //Console.WriteLine(reader.GetValue(1) + " " + reader.GetValue(2));
+                    StudentClass noviStudent = new StudentClass(reader.GetValue(0).ToString(), reader.GetValue(1).ToString(), reader.GetValue(2).ToString());
+                    students.Add(noviStudent);
+
+
+                }
+                reader.Close();
+                cmd = new SqlCommand("Select * from Jela", konekcija);
+
+                reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    //Console.WriteLine(reader.GetValue(0) + " " + reader.GetValue(1) + " " + reader.GetValue(2));
+                    JeloClass novoJelo = new JeloClass(reader.GetValue(1).ToString(), reader.GetValue(2).ToString(), reader.GetValue(3).ToString());
+                    jela.Add(novoJelo);
+                }
+                konekcija.Close();
+                reader.Close();
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                konekcija.Close();
+            }
+        }
     }
 }
